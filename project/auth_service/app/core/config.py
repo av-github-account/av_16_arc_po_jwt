e330ac2293f09e76fc859774b6f6eefa51ebe760
av <av.avramenko@gmail.com>
2025-05-02 00:20:46 +0300

    Changed ACCESS_TOKEN_EXPIRE_MINUTES=2

--- project/auth_service/app/core/config.py
+++ project/auth_service/app/core/config.py
@@ -3,4 +3,4 @@ import os
 DB_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/app")
 SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 1
+ACCESS_TOKEN_EXPIRE_MINUTES = 2
